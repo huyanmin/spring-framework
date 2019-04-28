564e377eca6e2a2d8a6839be00c6aa721421ec03
Ginny <516416745@qq.com>
2019-04-28 17:50:31 +0800

    init

---
+++ src/main/java/com/ginny/spring/framework/context/GPApplicationContext.java
@@ -0,0 +1,16 @@
+package com.ginny.spring.framework.context;
+
+import com.ginny.spring.framework.context.support.GPAbstractApplicationContext;
+
+/**
+ * @Author: Ginny
+ * @Desc: //TODO
+ * @Date:Create 2019/4/25 14:40
+ */
+public class GPApplicationContext extends GPAbstractApplicationContext {
+
+    @Override
+    public void refresh(){
+
+    }
+}
---
+++ src/main/java/com/ginny/spring/framework/context/support/GPAbstractApplicationContext.java
@@ -0,0 +1,11 @@
+package com.ginny.spring.framework.context.support;
+
+/**
+ * @Author: Ginny
+ * @Desc: //TODO
+ * @Date:Create 2019/4/25 13:52
+ */
+public abstract class GPAbstractApplicationContext  {
+
+    public void refresh() throws Exception{};
+}
